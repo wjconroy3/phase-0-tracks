d07758e259cb3af0fc9dbc86b25d2ebff93efa75
Bill Conroy <wjconroy3@gmail.com>
2016-11-02 13:53:54 -0700

    FINISHED EXERCISE

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -7,33 +7,57 @@
 # "iNvEsTiGaTiOn".<???>
 # => “InVeStIgAtIoN”
 
+puts "iNvEsTiGaTiOn".swapcase
+
 # "zom".<???>
 # => “zoom”
+
+puts "zom".insert(1, 'o')
 
 # "enhance".<???>
 # => "    enhance    "
 
+puts "enhance".center(15)
+
 # "Stop! You’re under arrest!".<???>
 # => "STOP! YOU’RE UNDER ARREST!"
+
+puts "Stop! You’re under arrest!".upcase
 
 # "the usual".<???>
 #=> "the usual suspects"
 
+puts "the usual".replace "the usual suspects"
+
 # " suspects".<???>
 # => "the usual suspects"
+puts " suspects".replace "the usual suspects"
+
+text = "the usual "
+puts text.to_s + "suspects"
 
 # "The case of the disappearing last letter".<???>
 # => "The case of the disappearing last lette"
 
+puts "The case of the disappearing last letter".chop
+
 # "The mystery of the missing first letter".<???>
 # => "he mystery of the missing first letter"
 
+puts "The mystery of the missing first letter".delete "T"
+
 # "Elementary,    my   dear        Watson!".<???>
 # => "Elementary, my dear Watson!"
+
+puts "Elementary,    my   dear        Watson!".squeeze( " ")
 
 # "z".<???>
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
+puts "z".ord
+
 # "How many times does the letter 'a' appear in this string?".<???>
 # => 4
+
+puts "How many times does the letter 'a' appear in this string?".count "a"
