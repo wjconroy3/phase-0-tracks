4f3ef45bb4d0927a346f2ba4bc2be30b75318c93
Bill Conroy <wjconroy3@gmail.com>
2016-11-07 12:32:12 -0800

    FINISHED RELEASE 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,3 +1,5 @@
+## Release 0: Add Behavior to a Class ##
+
 class Puppy
 
   def fetch(toy)
@@ -5,5 +7,35 @@ class Puppy
     toy
   end
 
+  def speak(x)
+  	x.times do
+  		puts "Woof!"
+  	end	
+  end
+
+  def roll_over() 
+  	puts "*Rolls over*"
+  end
+
+  def dog_years(x)
+  	x * 7
+  end
+
+  def protect()
+	puts "GGGRRRRRR!!!" 	
+  end
+
 end
+
+patches = Puppy.new
+
+patches.fetch("ball")
+
+patches.speak(5)
+
+patches.roll_over
+
+puts patches.dog_years(3)
+
+patches.protect()
 
