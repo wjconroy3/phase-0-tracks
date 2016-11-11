9b285d62b6aea4711706f20f145791420911c797
Bill Conroy <wjconroy3@gmail.com>
2016-11-11 15:24:05 -0800

    FINISHED RELEASE 3

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,20 +1,43 @@
-### Release 1: Write a Simple Module ###
+# ### Release 1: Write a Simple Module ###
 
-module Shout
-	def self.yell_angrily(words)
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+
+# 	def self.yell_happily(words)
+# 		words + "!!!" + " :)"
+# 	end
+# end
+
+# p Shout.yell_angrily("UGHH")
+# p Shout.yell_happily("Woohoo")
+# p Shout.yell_angrily("WHERE ARE MY KEYS??")
+
+
+# ### Release 3: Convert a Standalone Module to a Mixin ### 
+
+module Yell
+	def yell_angrily(words)
 		words + "!!!" + " :("
 	end
 
-	def self.yell_happily(words)
+	def yell_happily(words)
 		words + "!!!" + " :)"
-	end
+	end	
 end
 
-p Shout.yell_angrily("UGHH")
-p Shout.yell_happily("Woohoo")
-p Shout.yell_angrily("WHERE ARE MY KEYS??")
+class Boss
+	include Yell
+end
 
+class Girlfriend
+	include Yell
+end
 
-### Release 2: Mix in a Module ### 
+boss = Boss.new
+p boss.yell_angrily("YOU'RE LATE")
 
+girlfriend = Girlfriend.new
+p girlfriend.yell_happily("I missed you")
 
