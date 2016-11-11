eb209762daf6a146cfb76b16f8946fe37cab2155
Bill Conroy <wjconroy3@gmail.com>
2016-11-11 14:39:05 -0800

    FINISHED RELEASE 1

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,20 @@
+### Release 1: Write a Simple Module ###
+
 module Shout
-	# we'll put some methods here soon, but this code is fine for now!
+	def self.yell_angrily(words)
+		words + "!!!" + " :("
+	end
+
+	def self.yell_happily(words)
+		words + "!!!" + " :)"
+	end
 end
+
+p Shout.yell_angrily("UGHH")
+p Shout.yell_happily("Woohoo")
+p Shout.yell_angrily("WHERE ARE MY KEYS??")
+
+
+### Release 2: Mix in a Module ### 
+
+
